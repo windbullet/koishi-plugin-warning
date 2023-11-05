ae4f9eb26bea890ba24e2ea4354782d6743fb368
windbullet <2536810643@qq.com>
2023-11-05 15:04:26 +0800

    提交文件

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,103 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema, h } from 'koishi'
 
 export const name = 'warning'
 
 export const usage = "接收提醒的人需要是机器人的好友"
 
+export const inject = ['database']
+
+declare module 'koishi' {
+  interface Tables {
+    warningData: WarningData;
+  }
+}
+
+export interface WarningData {
+  id: number;
+  guildId: string;
+  keywords: string[];
+}
+
 export interface Config {
-  提醒关键词:string[];
-  接收提醒的人:string;
+
+  全局提醒关键词:string[];
+  超级管理员:string;
   自我触发:boolean;
 }
 
 export const Config: Schema<Config> = Schema.object({
-  提醒关键词:Schema.array(Schema.string())
+  全局提醒关键词:Schema.array(Schema.string())
     .description("群聊中有这些词时，会发送提醒"),
-  接收提醒的人:Schema.string()
-    .description("接收提醒的人的ID"),
+  超级管理员:Schema.string()
+    .description("接收提醒以及管理群聊关键词的人的ID"),
   自我触发:Schema.boolean()
-    .description("接收人触发关键词时是否提醒")
+    .description("超级管理员触发关键词时是否提醒")
     .default(false)
 })
 
 export function apply(ctx: Context, config: Config) {
+  extendTable(ctx)
   ctx.guild().middleware(async (session, next) => {
-    for (let i of config.提醒关键词) {
-      if (new RegExp(`${i}`).test(session.content) && (config.自我触发 ? true : session.event.user.id !== config.接收提醒的人)) {
-        const channel = await session.bot.createDirectChannel(config.接收提醒的人)
-        await session.bot.sendMessage(channel.id, `
+    const data = await ctx.database.get('warningData', { guildId: session.event.channel.id })
+    let result = []
+    for (let i of (data.length === 0 ? config.全局提醒关键词 : config.全局提醒关键词.concat(data[0].keywords))) {
+      if (new RegExp(`${i}`).test(session.content) && (config.自我触发 ? true : session.event.user.id !== config.超级管理员)) {
+        result.push(i)
+      }
+    }
+    if (result.length > 0) {
+    const channel = await session.bot.createDirectChannel(config.超级管理员)
+    await session.bot.sendMessage(channel.id, `
 【WARNING】
-触发关键词：${i}
+触发关键词：${result.join(", ")}
 触发群聊：${session.event.guild.name}(${session.event.channel.id})
 触发用户：${session.username}(${session.event.user.id})
-消息内容：
-${session.content}`)
-      }
+消息内容：${session.content}`)
     }
     return next()
     
   }), true
+
+  ctx.guild().command("warning.添加关键词 <keyword:string>")
+    .alias("添加提醒关键词")
+    .usage("添加针对该群的提醒关键词")
+    .example("添加提醒关键词 Koishi")
+    .action(async ({session}, keyword) => {
+      if (session.event.user.id === config.超级管理员) {
+        if (config.全局提醒关键词.includes(keyword)) return h("quote", session.event.message.id) + "该关键词已存在于全局提醒关键词"
+        const data = await ctx.database.get('warningData', { guildId: session.event.channel.id })
+        if (data.length === 0) {
+          await ctx.database.create('warningData', { guildId: session.event.channel.id, keywords: [keyword] })
+          return h("quote", session.event.message.id) + "添加成功"
+        }
+        if (data[0].keywords.includes(keyword)) return "该关键词已存在"
+        data[0].keywords.push(keyword)
+        await ctx.database.set('warningData', { guildId: session.event.channel.id }, { keywords: data[0].keywords })
+        return h("quote", session.event.message.id) + "添加成功"
+      }
+      return h("quote", session.event.message.id) + "你没有权限"
+      
+    })
+
+  ctx.guild().command("warning.删除关键词 <keyword:string>")
+    .alias("删除提醒关键词")
+    .usage("删除针对该群的提醒关键词")
+    .example("删除提醒关键词 Koishi")
+    .action(async ({session}, keyword) => {
+      if (session.event.user.id === config.超级管理员) {
+        const data = await ctx.database.get('warningData', { guildId: session.event.channel.id })
+        if (data.length === 0 || !data[0]?.keywords.includes(keyword)) return h("quote", session.event.message.id) + "本群无该提醒关键词"
+        await ctx.database.set('warningData', { guildId: session.event.channel.id }, { keywords: data[0].keywords.splice(data[0].keywords.indexOf(keyword), 1) })
+        return h("quote", session.event.message.id) + "删除成功"
+      }
+      return h("quote", session.event.message.id) + "你没有权限"
+    })
+}
+
+async function extendTable(ctx) {
+  await ctx.model.extend('warningData', {
+    id: "unsigned",
+    guildId: "string",
+    keywords: "list"
+  }, {primary: 'id', autoInc: true})
 }
