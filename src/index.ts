f0d44ed436948454153dd0c1e3ef43dee90f15b3
windbullet <2536810643@qq.com>
2024-02-07 14:12:24 +0800

    更新

--- src/index.ts
+++ src/index.ts
@@ -25,16 +25,18 @@ export interface WarningData {
 }
 
 export interface Config {
-
   全局提醒关键词:string[];
-  超级管理员:string;
+  撤回消息:boolean;
+  超级管理员:string[];
   自我触发:boolean;
 }
 
 export const Config: Schema<Config> = Schema.object({
   全局提醒关键词:Schema.array(Schema.string())
     .description("群聊中有这些词时，会发送提醒"),
-  超级管理员:Schema.string()
+  撤回消息:Schema.boolean()
+    .description("是否撤回触发关键词的消息（机器人需要是管理员）"),
+  超级管理员:Schema.array(Schema.string())
     .description("接收提醒以及管理群聊关键词的人的ID"),
   自我触发:Schema.boolean()
     .description("超级管理员触发关键词时是否提醒")
@@ -47,18 +49,24 @@ export function apply(ctx: Context, config: Config) {
     const data = await ctx.database.get('warningData', { guildId: session.event.channel.id })
     let result = []
     for (let i of (data.length === 0 ? config.全局提醒关键词 : config.全局提醒关键词.concat(data[0].keywords))) {
-      if (new RegExp(`${i}`).test(session.content) && (config.自我触发 ? true : session.event.user.id !== config.超级管理员)) {
+      if (new RegExp(`${i}`).test(session.content) && (config.自我触发 ? true : !config.超级管理员.includes(session.event.user.id))) {
         result.push(i)
       }
     }
     if (result.length > 0) {
-    const channel = await session.bot.createDirectChannel(config.超级管理员)
-    await session.bot.sendMessage(channel.id, `
+      if (config.撤回消息) {
+        await session.bot.deleteMessage(session.event.channel.id, session.event.message.id)
+      }
+      for (let admin of config.超级管理员) {
+        const channel = await session.bot.createDirectChannel(admin)
+        await session.bot.sendMessage(channel.id, `
 【WARNING】
 触发关键词：${result.join(", ")}
 触发群聊：${session.event.channel.name}(${session.event.guild.id})
 触发用户：${session.username}(${session.event.user.id})
 消息内容：${session.content}`)
+      }
+
     }
     return next()
     
@@ -69,7 +77,7 @@ export function apply(ctx: Context, config: Config) {
     .usage("添加针对该群的提醒关键词")
     .example("添加提醒关键词 Koishi")
     .action(async ({session}, keyword) => {
-      if (session.event.user.id === config.超级管理员) {
+      if (config.超级管理员.includes(session.event.user.id)) {
         if (config.全局提醒关键词.includes(keyword)) return h("quote", session.event.message.id) + "该关键词已存在于全局提醒关键词"
         const data = await ctx.database.get('warningData', { guildId: session.event.channel.id })
         if (data.length === 0) {
@@ -90,7 +98,7 @@ export function apply(ctx: Context, config: Config) {
     .usage("删除针对该群的提醒关键词")
     .example("删除提醒关键词 Koishi")
     .action(async ({session}, keyword) => {
-      if (session.event.user.id === config.超级管理员) {
+      if (config.超级管理员.includes(session.event.user.id)) {
         const data = await ctx.database.get('warningData', { guildId: session.event.channel.id })
         if (data.length === 0 || !data[0]?.keywords.includes(keyword)) return h("quote", session.event.message.id) + "本群无该提醒关键词"
         await ctx.database.set('warningData', { guildId: session.event.channel.id }, { keywords: data[0].keywords.splice(data[0].keywords.indexOf(keyword), 1) })
